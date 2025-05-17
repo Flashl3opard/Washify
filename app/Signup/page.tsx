1398b92e94820833dbb0353d28092cf0f54e19f7
Flashl3opard <sheoreyyash@gmail.com>
2025-05-17 21:52:04 +0530

    Changes in Signup Page

--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -38,12 +38,12 @@ const Page = () => {
 
           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
-              Email Address
+              Phone Number
             </label>
             <input
-              type="email"
+              type="number"
               className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
-              placeholder="Enter your Email Address"
+              placeholder="Enter your Mobile Number"
             />
           </div>
 
